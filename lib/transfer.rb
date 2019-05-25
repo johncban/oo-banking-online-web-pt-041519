96b084d66e7ffe2502e00cf8b91e1c096b4b253b
johncban <johncban@gmail.com>
2019-05-25 21:40:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
+attr_accessor :status
+
 class Transfer
   # your code here
   def initialize(acsender, acreceiver)
     @sender = acsender
     @receiver = acreceiver
+    @status = "pending"
   end
 end
