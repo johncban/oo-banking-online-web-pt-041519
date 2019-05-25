38e46161dce71ea8f5f7e170ac849fc74d4514c3
johncban <johncban@gmail.com>
2019-05-25 20:48:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,9 @@
 class BankAccount
 
-  def initialize(name, balance, status)
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
-  end
-
-  def name
-    @name
   end
 
 end
